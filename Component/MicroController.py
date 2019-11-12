ff1e1a3dd71797f1cfeb7bdcd4472d549a818435
amrishAK <aamrish007@live.com>
2019-11-12 16:55:29 +0000

    added basic microCOntroller functions

--- Component/MicroController.py
+++ Component/MicroController.py
@@ -1,4 +1,44 @@
 from Helper.JsonHandler import JsonHandler
 
 class MicroController (object) : 
-    pass
+    
+    characteristicsPath = "Characteristics/MicroController.json"
+    _inputVoltage = 0
+    _coreCurrent = 0
+
+    def __init__ (self,inputVoltage) :
+        self.jsonHandler = JsonHandler()
+        self.ControllerChar = self.jsonHandler.LoadJson(self.characteristicsPath)
+        self._inputVoltage = inputVoltage
+        self._coreCurrent = self.ControllerChar['Current']['Mode']['Run']
+
+ 
+    def TurnOn(self) :
+        self.ToActiveMode()
+
+    def TurnOff(self):
+        #updatePower
+        pass   
+    
+    def ToActiveMode(self):
+        self._coreCurrent = self.ControllerChar['Current']['Mode']['Run']
+
+    def ToIdleMode(self):
+        self._coreCurrent = self.ControllerChar['Current']['Mode']['Idle']
+
+    def ToSleepMode(self):
+        self._coreCurrent = self.ControllerChar['Current']['Mode']['Sleep']
+
+    def I2CRead(self):
+        Temp = 12
+        self.I2CPowerConsumed()
+        return Temp
+
+    def I2CWrite(self):
+        self.I2CPowerConsumed()
+
+    def I2CPowerConsumed(self):
+        time = (self.ControllerChar['BitSize'] / self.ControllerChar['BitRate'])/3600 # bitrate is in seconds, convert it to hours
+        power = time * self._inputVoltage * self._coreCurrent 
+        return power
+    
